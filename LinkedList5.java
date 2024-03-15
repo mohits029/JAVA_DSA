ec365e50eae3e2bab679b164d3ab2530207ca26b
Mohit Sarkar <mohitsarkar256@gmail.com>
2024-03-15 19:14:01 +0530

    Hello world

---
+++ LinkedList5.java
@@ -0,0 +1,62 @@
+public class LinkedList5 {
+    
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data= data;
+            this.next= null;
+        }
+    }
+    static Node head;
+    static Node tail;
+
+    public void addLast(int n){
+        if(head== null){
+            Node newNode= new Node(n);
+            head= newNode;
+            tail= newNode;
+        }
+        else{
+            Node newNode= new Node(n);
+            tail.next= newNode;
+            tail= newNode;
+        }
+    }
+
+
+    public void addFirst(int n){
+        Node newNode= new Node(n);
+        
+        if(head== null){
+            head= newNode;
+            tail= newNode;
+            
+        }else{
+            newNode.next= head;
+            head= newNode;
+        }
+    }
+
+    public void display(Node curr){
+        while (curr!= null) {
+            System.out.print(curr.data+" -> ");
+            curr= curr.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList5 list1= new LinkedList5();
+        list1.addLast(10);
+        list1.addLast(20);
+        list1.addLast(30);
+        list1.addLast(40);
+
+        list1.addFirst(500);
+        list1.addFirst(600);
+
+        list1.display(list1.head);
+
+    }
+}
